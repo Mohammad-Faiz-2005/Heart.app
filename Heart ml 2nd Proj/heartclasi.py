4e63eefc9d63b36cce034a8dbacdc634225eb2e0
Mohammad-Faiz-2005 <mfaiz313313@gmail.com>
2025-09-06 01:27:14 +0530

    Update heartclasi.py

--- Heart ml 2nd Proj/heartclasi.py
+++ Heart ml 2nd Proj/heartclasi.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -12,7 +13,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 st.header("Heart Disease Prediction Model")
 
-dataset = pd.read_csv("Heart ml 2nd Proj/framingham.csv")
+dataset = pd.read_csv(r"C:\Users\Mohammad Faiz\Downloads\framingham.csv")
 df = pd.DataFrame(dataset)
 
 #df
@@ -48,30 +49,62 @@ st.write("The model works with ",vt.score(x_test , y_test)*100," Accuracy.")
 
 vt.predict(x_test)
 
-Gender = st.number_input("Enter Gender  1 for Male and 0 for Female: ")
-Age = st.number_input("Enter the Age: ")
-Smoker = st.number_input("Are you a Smoker?  1 for Yes and 0 for No: ")
-if(Smoker == 1):
-    cigsPerDay = st.number_input("Enter how many cigarettes you consume daily: ")
+Gen = st.radio("Genter:",["Male" , "Female"])
+#Gender = st.number_input("Enter Gender  1 for Male and 0 for Female: ")
+if Gen=="Male":
+    Gender = 1
 else:
+    Gender = 0
+
+Age = st.number_input("Enter the Age: ")
+if Age <0 or Age > 120 :
+    st.warning("Invalid Input !!")
+
+Smoker = 0    
+Smok = st.radio("Are you a Smoker:",["YES","NO"])
+if(Smok == "YES"):
+    Smoker = 1
+    cigsPerDay = st.number_input("Enter how many cigarettes you consume daily: ")
+    if cigsPerDay < 0:
+        st.error("Invalid Input")
+elif Smok=="No":
     cigsPerDay = 0
+
 BP = st.number_input("Enter BP MEDS: ")
-PreStroke = st.number_input("Are you with prevalentStroke?  1 for Yes and 0 for No: ")
-preHyp = st.number_input("Is your Blood Presure High?  1 for Yes and 0 for No: ")
-diabetes = st.number_input("Are you have Diabetes?  1 for Yes and 0 for No: ")
+PreSt = st.radio("Are you with prevalentStroke?",["YES","No"])
+if PreSt=="Yes":
+    PreStroke = 1
+else:
+    PreStroke = 0
+
+preH = st.radio("Is your Blood Presure High: ",["YES","NO"])
+if preH=="Yes":
+    preHyp = 1
+else:
+    preHyp = 0
+
+diabe = st.radio("Are you have Diabetes: ",["YES","NO"])
+if diabe=="Yes":
+    diabetes = 1
+else:
+    diabetes = 0
+
 totChol = st.number_input("Enter Total Cholistrol: ")
+
 sysBP = st.number_input("Enter Systolic Blood Pressure: ")
+
 diaBP = st.number_input("Enter  Diastolic Blood Pressure: ")
+
 BMI = st.number_input("Enter Body Mass Index: ")
+
 HR = st.number_input("Enter Heart Rate: ")
+
 Glu = st.number_input("Enter Glucose: ")
 
 if st.button("Give Prediction"):
+    st.balloons()
     prediction = vt.predict([[Gender , Age , Smoker , cigsPerDay , BP , PreStroke ,preHyp , diabetes , totChol , sysBP , diaBP , BMI , HR , Glu]])
     if prediction ==0:
         st.header("Prediction is : You Are Safe With No Heart Disease. ")
     else:
         st.header("Prediction is : You Have A Heart Disease. Please consult a doctor for further evaluation.")
-
-
-
